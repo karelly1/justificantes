f4df728dc17b78adee192dd21b78569872cafaab
karelly1 <145032006+karelly1@users.noreply.github.com>
2025-10-07 17:12:56 -0600

    Update db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,10 +1,9 @@
-// backend/src/db.js
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'localhost',        // localhost para dev local, "mysql" en Actions
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'mysql123',
+  password: process.env.DB_PASSWORD || 'mysql123', // local dev
   database: process.env.DB_NAME || 'justificantes_db',
   waitForConnections: true,
   connectionLimit: 10,
@@ -12,3 +11,4 @@ const pool = mysql.createPool({
 });
 
 module.exports = pool;
+
